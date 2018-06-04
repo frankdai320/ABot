9bbd6995167701d27f2f0a5c196bf064c1b566ea
frankdai320 <frankdai320@gmail.com>
2018-06-03 17:13:27 -0700

    Add index to urban dictionary command

--- urban_dict.py
+++ urban_dict.py
@@ -3,11 +3,14 @@ import requests
 API_PATH = 'http://api.urbandictionary.com/v0/define'
 
 
-def build_message(term):
+def build_message(term, index=1):
     ud_response = requests.get(API_PATH, params={'term': term}).json()
     if ud_response['result_type'] != 'exact':  # unknown how many possible responses the API gives here
         return 'No definition found for {}.'.format(term)
-    entry = ud_response['list'][0]
+    try:
+        entry = ud_response['list'][index-1]
+    except IndexError:
+        return "Error retrieving entry number {} for term {}".format(term, index)
     word = entry['word']
     definition = entry['definition']
     example = entry['example']
